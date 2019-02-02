f5a9eb5f8a110be657a6d5b5d95c1dec33f37ade
WebD01 <liangu@protonmail.com>
2019-02-02 10:29:40 -0500

    refractor code

--- euler/PermutedMultiples.hs
+++ euler/PermutedMultiples.hs
@@ -5,23 +5,19 @@ digits :: Integral x => x -> [x]
 digits 0 = []
 digits x = digits (x `div` 10) ++ [x `mod` 10]
 
-notEqualLength :: Int -> Int -> Bool
+notEqualLength :: Integer -> Integer -> Bool
 notEqualLength n1 n2 = length (digits n1) /= length (digits n2)
 
-notSameDigits :: Int -> Int -> Bool
+notSameDigits :: Integer -> Integer -> Bool
 notSameDigits n1 n2 = Set.size (Set.difference (Set.fromList (digits n2)) (Set.fromList (digits n1))) /= 0
 
-findInteger :: Int -> Int
-findInteger 0 = findInteger (1) 
+checkForMultiples :: (Integer -> Integer -> Bool) -> Integer -> Integer -> Bool
+checkForMultiples op n k = or [ n `op` (n * i) | i <- [2..k] ]
+
+findInteger :: Integer -> Integer
+findInteger 0 = findInteger 1
 findInteger n
-    | notEqualLength n (n * 2) = findInteger (n + 1)
-    | notEqualLength n (n * 3) = findInteger (n + 1)
-    | notEqualLength n (n * 4) = findInteger (n + 1)
-    | notEqualLength n (n * 5) = findInteger (n + 1)
-    | notEqualLength n (n * 6) = findInteger (n + 1)
-    | notSameDigits n (n * 2) = findInteger (n + 1)
-    | notSameDigits n (n * 3) = findInteger (n + 1)
-    | notSameDigits n (n * 4) = findInteger (n + 1)
-    | notSameDigits n (n * 5) = findInteger (n + 1)
-    | notSameDigits n (n * 6) = findInteger (n + 1)
+    | cond      = findInteger (n + 1)
     | otherwise = n
+    where cond = checkForMultiples notEqualLength n 6
+              || checkForMultiples notSameDigits n 6 
